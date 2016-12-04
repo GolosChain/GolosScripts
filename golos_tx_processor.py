5a97b9dd764fe8e1e6f1813cc3dd9e1ab77b35b7
Valery Litvin <litvintech@gmail.com>
2016-12-04 02:32:06 +0200

    [scripts][update] Updated transfers scripts with exception handlers

--- golos_tx_processor.py
+++ golos_tx_processor.py
@@ -1,5 +1,6 @@
 import json
 from time import time
+import traceback, logging
 from piston.steem import (
     Steem,
     BroadcastingError
@@ -14,20 +15,31 @@ transfer_list = json.load(open('./step_two_init_tx_golos.json'))
 # transfer_list = json.load(open('./step_seven_sharedrop_tx_golos.json'))
 logs          = []
 
-for index, tx in enumerate(transfer_list):
-    try:
-        transfer_log = steem.transfer(account=creator,
-                                      amount=tx['amount'],
-                                      asset="GOLOS",
-                                      to=tx['account'],
-                                      memo="")
-        logs.append(transfer_log)
-        print ("[+] <{0}> Transfered {1} to {2}".format(index+1, tx['amount'], tx['account']))
-    except BroadcastingError:
-        print ("[-] <{0}> Didn't transferred {1} to {2}".format(index+1, tx['amount'], tx['account']))
-        pass
-
-json.dump(logs, open('golos_transfer_log_' + str(int(time())) + '.json', 'w'),
-                                                                    indent=4,
-                                                                    sort_keys=True,
-                                                                    separators=(',', ':'))
+print("[-] Started golos transfer operations")
+try:
+    for index, tx in enumerate(transfer_list):
+        try:
+            transfer_log = steem.transfer(account=creator,
+                                          amount=tx['amount'],
+                                          asset="GOLOS",
+                                          to=tx['account'],
+                                          memo="")
+            logs.append(transfer_log)
+            print ("[+] <{0}> Transfered {1} to {2}".format(index+1, tx['amount'], tx['account']))
+        except BroadcastingError:
+            print ("[-] <{0}> Didn't transferred {1} to {2}".format(index+1, tx['amount'], tx['account']))
+            pass
+except KeyboardInterrupt as e:
+    print("[!] Raised keyboard interrupt...")
+    raise
+except Exception as e:
+    print("[!] Raised unrecognized error...")
+    logging.error(traceback.format_exc())
+else:
+    print("[+] Finished golos transfer operations")
+finally:
+    json.dump(logs, open('golos_transfer_log_' + str(int(time())) + '.json', 'w'),
+                                                                        indent=4,
+                                                                        sort_keys=True,
+                                                                        separators=(',', ':'))
+    print("[*] Script finished")
--- golospower_tx_processor.py
+++ golospower_tx_processor.py
@@ -1,5 +1,6 @@
 import json
 from time import time
+import traceback, logging
 from piston.steem import (
     Steem,
     BroadcastingError
@@ -15,18 +16,29 @@ transfer_list = json.load(open('./step_three_init_tx_power.json'))
 # transfer_list = json.load(open('./step_six_sharedrop_tx_power.json'
 logs          = []
 
-for index, tx in enumerate(transfer_list):
-    try:
-        transfer_log = steem.transfer_to_vesting(account=creator,
-                                                 amount=tx['amount'],
-                                                 to=tx['account'])
-        logs.append(transfer_log)
-        print ("[+] <{0}> Transfered {1} to {2}".format(index+1, tx['amount'], tx['account']))
-    except BroadcastingError:
-        print ("[-] <{0}> Didn't transferred {1} to {2}".format(index+1, tx['amount'], tx['account']))
-        pass
-
-json.dump(logs, open('golospower_transfer_log_' + str(int(time())) + '.json', 'w'),
-                                                                    indent=4,
-                                                                    sort_keys=True,
-                                                                    separators=(',', ':'))
+print("[+] Started golos power transfer operations")
+try:
+    for index, tx in enumerate(transfer_list):
+        try:
+            transfer_log = steem.transfer_to_vesting(account=creator,
+                                                     amount=tx['amount'],
+                                                     to=tx['account'])
+            logs.append(transfer_log)
+            print ("[+] <{0}> Transfered {1} to {2}".format(index+1, tx['amount'], tx['account']))
+        except BroadcastingError:
+            print ("[-] <{0}> Didn't transferred {1} to {2}".format(index+1, tx['amount'], tx['account']))
+            pass
+except KeyboardInterrupt as e:
+    print("[!] Raised keyboard interrupt...")
+    raise
+except Exception as e:
+    print("[!] Raised unrecognized error...")
+    logging.error(traceback.format_exc())
+else:
+    print("[+] Finished golos power transfer operations")
+finally:
+    json.dump(logs, open('golospower_transfer_log_' + str(int(time())) + '.json', 'w'),
+                                                                        indent=4,
+                                                                        sort_keys=True,
+                                                                        separators=(',', ':'))
+    print("[*] Script finished")
